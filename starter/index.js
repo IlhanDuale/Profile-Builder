efe1ec6277bec2a110e2e767513d196da70597c4
Ilhan <dualeilhan@gmail.com>
2023-12-30 12:02:41 +0000

    engineer intern and manager classes defined, index.js completed

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,145 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// index.js
+
+function promptManager() {
+    inquirer
+        .prompt([{
+                type: 'input',
+                name: 'name',
+                message: "Enter the team manager's name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter the team manager's employee ID:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter the team manager's email:",
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "Enter the team manager's office number:",
+            },
+        ])
+        .then((answers) => {
+            const manager = new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.officeNumber
+            );
+            teamMembers.push(manager);
+            promptMenu();
+        });
+}
+
+function promptEngineer() {
+    inquirer
+        .prompt([{
+                type: 'input',
+                name: 'name',
+                message: "Enter the engineer's name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter the engineer's employee ID:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter the engineer's email:",
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "Enter the engineer's GitHub username:",
+            },
+        ])
+        .then((answers) => {
+            const engineer = new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.github
+            );
+            teamMembers.push(engineer);
+            promptMenu();
+        });
+}
+
+function promptIntern() {
+    inquirer
+        .prompt([{
+                type: 'input',
+                name: 'name',
+                message: "Enter the intern's name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter the intern's employee ID:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter the intern's email:",
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "Enter the intern's school:",
+            },
+        ])
+        .then((answers) => {
+            const intern = new Intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.school
+            );
+            teamMembers.push(intern);
+            promptMenu();
+        });
+}
+
+function promptMenu() {
+    inquirer
+        .prompt([{
+            type: 'list',
+            name: 'menuChoice',
+            message: 'What would you like to do?',
+            choices: ['Add an engineer', 'Add an intern', 'Finish building the team'],
+        }, ])
+        .then((answer) => {
+            switch (answer.menuChoice) {
+                case 'Add an engineer':
+                    promptEngineer();
+                    break;
+                case 'Add an intern':
+                    promptIntern();
+                    break;
+                case 'Finish building the team':
+                    generateHTML();
+                    break;
+                default:
+                    break;
+            }
+        });
+}
+
+function generateHTML() {
+    const renderedHtml = render(teamMembers);
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, renderedHtml);
+    console.log(`HTML file generated successfully at ${outputPath}`);
+}
+
+promptManager();
